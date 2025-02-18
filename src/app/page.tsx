5b9759a0db0deb65df140c6cd0fa6c6d8efeb9c4
alinnb <mailzyy@qq.com>
2025-02-18 15:25:06 +0800

    通过未缓存的数据来实现把页面编译成动态页面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { cookies } from "next/headers";
+
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  // 修复类型错误：cookies() 返回的是 ReadonlyRequestCookies 类型
-  const token = await cookieStore.get('token');
-
-  console.log('toLocaleTimeString 😀', token);
+  const r = await(await fetch('https://jsonplaceholder.typicode.com/todos/1',{
+    cache: 'no-cache',
+  })).json()
+  console.log(r);
+  console.log('toLocaleTimeString 😀');
   return (
     <div>
       <h1>{new Date().toLocaleTimeString()}</h1>
