63ee24d77c377197d93931392d18bb1cdb7548b7
alinnb <mailzyy@qq.com>
2025-02-18 16:18:49 +0800

    代码清理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 
-async function fetchImg () {
-  const r = await (await fetch('http://dog.ceo/api/breeds/image/random', {
-    cache: 'force-cache', 
-    next: {
-        tags: ["dog"],
-    },
-  })).json()
-  return r
-}
-
 export default async function Page() {
-  const img = await fetchImg()  
   console.log('😀');
   return (
     <div>
-      <img src={img.message} alt="Random dog" />
+      Home
     </div>
   );
 }
