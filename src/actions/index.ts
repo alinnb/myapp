306a2da20dc00e2c2917c7c5b2797f4a89ec5d44
alinnb <mailzyy@qq.com>
2025-02-19 09:43:34 +0800

    传递额外参数01

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,8 +8,9 @@ export async function getTodo() {
     return data
 }
 
-export async function addTodo(fromData: FormData) {
+export async function addTodo(userId:string, fromData: FormData) {
     const rawFormdata = Object.fromEntries(fromData)
+    console.log(userId)
     console.log(rawFormdata)
 
     data.push(rawFormdata.todo as string)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { addTodo, getTodo } from "@/actions";
 
-export default async function Page() {
-  console.log('😀');
+const userID = '123';
 
+export default async function Page() {
   const data:string[] = await getTodo();
 
   return (
     <div className="p-10">
-      <form action={addTodo}>
+      <form action={async (formData) => {
+        'use server'
+        await addTodo(userID, formData);
+      }}>
         <input type="text" name="todo" className="border p-2"/>
         <button type="submit" className="border p-2">提交</button>
       </form>
