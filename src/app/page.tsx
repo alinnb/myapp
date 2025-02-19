c78daa4d74201b4906538deef736e339f0b79f1c
alinnb <mailzyy@qq.com>
2025-02-19 09:46:43 +0800

    用bind形式添加action额外参数

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ const userID = '123';
 
 export default async function Page() {
   const data:string[] = await getTodo();
+  const addTodoWithId = addTodo.bind(null, userID);
 
   return (
     <div className="p-10">
-      <form action={async (formData) => {
-        'use server'
-        await addTodo(userID, formData);
-      }}>
+      <form action={addTodoWithId}>
         <input type="text" name="todo" className="border p-2"/>
         <button type="submit" className="border p-2">提交</button>
       </form>
